ba96c3bd840a41ca1be1c8034a4b16209075ecad
hzk <1304669328@qq.com>
2020-07-14 20:48:27 +0800

    dubbo-rpc-demo

---
+++ src/main/java/framework/Protocol.java
@@ -0,0 +1,11 @@
+package framework;
+
+import org.apache.dubbo.common.extension.SPI;
+
+@SPI
+public interface Protocol {
+
+    void start(URL url);
+
+    String send(URL url,Invocation invocation);
+}
